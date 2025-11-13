0d31d358030399f42cca236aa182b8d355509c28
Davide Ponzini <davide.ponzini95@gmail.com>
2025-11-13 12:24:00 +0100

    tests for CTE and subq

--- tests/detectors/1_syn/test_015_nested_aggregate_functions.py
+++ tests/detectors/1_syn/test_015_nested_aggregate_functions.py
@@ -1,61 +1,46 @@
 from tests import *
+import pytest
 
-def test_nested_aggregate_functions():
-    agg1 = 'SUM(MAX(price))'
-    agg2 = 'AVG(MIN(quantity))'
-    no_agg = 'SUM(price)'
-
-    query = f'''SELECT col1, {agg1}, {no_agg} FROM sales GROUP BY col1 HAVING {agg2};'''
-
+@pytest.mark.parametrize('query, aggregates', [
+    (
+        'SELECT col1, SUM(MAX(price)), SUM(price) FROM sales GROUP BY col1 HAVING AVG(MIN(quantity));',
+        ['SUM(MAX(price))', 'AVG(MIN(quantity))']
+    ),
+    # subqueries
+    (
+        'SELECT col1, COUNT(SUM(price)) FROM (SELECT col1, AVG(COUNT(quantity)) AS total_count FROM sales GROUP BY col1 ) AS subquery GROUP BY col1;',
+        ['COUNT(SUM(price))', 'AVG(COUNT(quantity))']
+    ),
+    # CTEs
+    (
+        'WITH cte AS (SELECT col1, MAX(SUM(price)) AS total_value FROM sales GROUP BY col1) SELECT col1, MIN(AVG(quantity)) FROM cte GROUP BY col1;',
+        ['MAX(SUM(price))', 'MIN(AVG(quantity))']
+    ),
+])
+def test_wrong(query, aggregates):
     detected_errors = run_test(
         query=query,
         detectors=[SyntaxErrorDetector],
     )
 
-    assert count_errors(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED) == 2
-    assert has_error(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED, (agg1,))
-    assert has_error(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED, (agg2,))
+    assert count_errors(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED) == len(aggregates)
+    for agg in aggregates:
+        assert has_error(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED, (agg,))
 
-def test_no_nested_aggregate_functions_subquery():
-    agg_in_subquery = 'MAX(price)'
-    outer_agg = 'SUM(total_price)'
-
-    query = f'''
-    SELECT col1, {outer_agg} FROM (
-        SELECT col1, {agg_in_subquery} AS total_price
-        FROM sales
-        GROUP BY col1
-    ) AS subquery
-    GROUP BY col1;
-    '''
-
+@pytest.mark.parametrize('query', [
+    'SELECT col1, SUM(total_price) FROM table1 GROUP BY col1;',
+    # subqueries
+    'SELECT col1, COUNT(quantity) FROM (SELECT col1, SUM(price) AS total FROM sales GROUP BY col1) AS subquery GROUP BY col1;',
+    'SELECT col1, AVG(total_quantity) FROM table1 WHERE total_quantity > (SELECT MAX(quantity) FROM table2) GROUP BY col1;',
+    # CTEs
+    'WITH cte AS (SELECT col1, MAX(price) AS max_price FROM sales GROUP BY col1) SELECT col1, MIN(max_price) FROM cte GROUP BY col1;',
+])
+def test_correct(query):
     detected_errors = run_test(
         query=query,
         detectors=[SyntaxErrorDetector],
     )
 
     assert count_errors(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED) == 0
-
-def test_aggregate_functions_subquery():
-    agg_in_subquery = 'COUNT(SUM(price))'
-    outer_agg = 'AVG(COUNT(quantity))'
-
-    query = f'''
-    SELECT col1, {outer_agg} FROM (
-        SELECT col1, {agg_in_subquery} AS total_count
-        FROM sales
-        GROUP BY col1
-    ) AS subquery
-    GROUP BY col1;
-    '''
-
-    detected_errors = run_test(
-        query=query,
-        detectors=[SyntaxErrorDetector],
-    )
-
-    assert count_errors(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED) == 2
-    assert has_error(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED, (agg_in_subquery,))
-    assert has_error(detected_errors, SqlErrors.SYN_15_AGGREGATE_FUNCTIONS_CANNOT_BE_NESTED, (outer_agg,))
 
 
