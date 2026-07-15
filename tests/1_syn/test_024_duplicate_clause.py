ea4bc4b25721bdd02b3adcd5ee2c3fbda3e7d6d8
Davide Ponzini <davide.ponzini95@gmail.com>
2026-07-15 12:38:46 +0200

    added test

--- tests/1_syn/test_024_duplicate_clause.py
+++ tests/1_syn/test_024_duplicate_clause.py
@@ -8,6 +8,7 @@ ERROR = SqlErrors.DUPLICATE_CLAUSE
     'SELECT col1 FROM table1 WHERE col2 = 10 GROUP BY col1 HAVING COUNT(*) > 1',
     'SELECT col1 FROM table1 WHERE col2 IN (SELECT col3 FROM table2) GROUP BY col1 HAVING COUNT(*) > 1',
     'SELECT col1 FROM table1 JOIN table2 ON table1.id = table2.id JOIN table3 ON table2.id = table3.id',
+    'SELECT SUM(col1) FILTER (WHERE col2 = 10) FROM table1 WHERE col3 = 20',
     'SELECT col1, COUNT(col2) FILTER (WHERE col3 = 1) as count_col FROM table1 WHERE col4 = 2',
     '''
         SELECT s.matricola, COUNT(e.voto), EXTRACT(YEAR FROM e.data) AS year, EXTRACT(MONTH FROM e.data) AS month
